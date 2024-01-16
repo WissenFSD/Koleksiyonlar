c399a44081afe2c8e2f8ecddbacbde856e0ee642
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-16 20:03:49 +0300

    stack koleksiyonu eklendi

---
+++ SortedList_Koleksiyonu/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+
+SortedList sorted =new SortedList();
+sorted.Add("06", "Ankara");
+sorted.Add("34", "İstanbul");
+sorted.Add("02", "Adıyamaman");
+sorted.Add("01", "Adana");
+
+
+// yeni eleman ekleme(Key alanı benzersiz olmalıdır.)
+//sorted.Add("01", "Adanaa");
+
+
+// int olarak giriyoruz(Veri bütünlüğünü korumamız gerekmektedir.)
+//sorted.Add(1, "Elma");
+
+
+// Değer güncelleme yapılabilir.
+sorted["01"] = "Bursa";
+
+
+// SortedList sıralı liste koleksiyonudur. İçerisine eklenen elemanları sıraya göre dizer. En küçük eleman her zaman koleksiyonun en üst sırasında yer almakdadır
+// Ekrana yazdırma
+foreach (DictionaryEntry item in sorted)
+{
+    Console.WriteLine("{0}- {1}",item.Key,item.Value);
+}
+
+
---
+++ Stack_Koleksiyonu/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+
+// İlk Giren son çıkar koleksiyonu(FİLO)
+Stack stack = new Stack();
+
+
+// Push metodu koleksiyona ekleme yapacaktır.
+stack.Push("Renault");
+stack.Push("Audi");
+stack.Push("BMW");
+
+Console.WriteLine("Koleksiyonumun eleman değeri : {0}",stack.Count);
+
+
+// Peek metodu bize bir sonraki silinecek olan değeri verir.Peek metodu koleksiyondan araba çıkartmaz. Sadece çıkacak olanı söyler.
+// Listenin içi boşken peek çalıştırılırsa exception verir.
+string cikacakAraba = stack.Peek().ToString();
+
+
+// Pop listeden elemanı çıkartacaktır. 
+string cikanAraba=stack.Pop().ToString();
+
+Console.WriteLine(cikacakAraba);
+
+
+
+
+
